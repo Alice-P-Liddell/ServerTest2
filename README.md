73c5b5b02f52fbe85608fdd28a4d6cc2efb1297c
Daehan Lee <alice.p.liddell0v0@gmail.com>
2019-12-30 17:23:22 +0900

    Set Basic
    +Build package.json
    
    README.md
    +Update How to use Json
    
    app.js
    +Build MongoClident Connect Func

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # ServerTest2
+
+
+
+### 점수 등록하기
+[POST] /users/scoreupdate
+
+<pre>
+{
+  'score':00
+}
+</pre>
+
+#### 성공 시
+<pre>
+{
+  'message':'Updated your Best Score'
+  'score':00
+}
+</pre>
+
+#### 실패 시
+<pre>
+{
+  'message':'400 Bad Request'
+}
+</pre>
+
+
+
+### 점수 불러오기
+[GET] /users/scoreload
+
+<pre>
+{
+  'username':'hongildong',
+  'score':00
+}
+</pre>
+
+#### 성공 시
+<pre>
+{
+  'username':'hongildong',
+  'score':00
+}
+</pre>
+
+#### 로그인 실패 시
+<pre>
+{
+  'message':'Not Found Userdata'
+}
+</pre>
+
+#### 기타 실패 시
+<pre>
+{
+  'message':'400 Bad Request'
+}
+</pre>
---
+++ app.js
@@ -0,0 +1,54 @@
+/* 테스트용 헬로월드(로컬호스트)
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+*/
+
+//몽고 클라이언트
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'ServerTest2';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
+});
+
+/*
+//인서트 도큐먼트
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+    collection.insertMany([
+      {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {
+      assert.equal(err, null);
+      assert.equal(3, result.result.n);
+      assert.equal(3, result.ops.length);
+      console.log("Inserted 3 documents into the collection");
+      callback(result);
+    });
+}
+*/
